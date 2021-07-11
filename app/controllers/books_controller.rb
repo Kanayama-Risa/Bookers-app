3cd184460caf493a2d63e25feb76c6f8d447032b
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-11 07:59:20 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,25 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/save'
+  end
+
+  
+  def show
+    @list = Book.find(params[:id])
+  end
+  
+  
+  
+  private
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books/index'
+  get 'book' => 'books#index'
+  get 'new' => 'books#new'
+  post 'books' => 'books#create'
   get 'books/show'
-  get 'books/new'
   get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
+
