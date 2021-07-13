db5f7cdcd308666782ecfb4cf0b1463416691bec
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-13 05:06:12 +0000

    [Update]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # get 'books' => 'books#index'
   # get 'books/:id' => 'books#show', as: 'book'
   # get 'books/:id/edit' => 'books#edit'
-   resources :books
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
