f6ef8635d08c02681f7a5045a00bc4304efe22d6
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-13 06:31:18 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update
+    @book.update(book_params)
     redirect_to book_path(@book)
   end
   
